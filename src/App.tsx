cf63cdb4aa7f09e871f46d76b7608901e54739ad
geforseN <semen.neb@gmail.com>
2022-08-17 17:16:37 +0700

    add navigation and sign-in components

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,16 @@
+import {Route, Routes} from "react-router-dom";
 import './App.css';
 
-import {Outlet, Route, Routes} from "react-router-dom";
 import Home from "./routes/home/home.component";
-
-
-const Navigation = () => {
-  return (
-    <nav>
-      <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-      </ul>
-      <Outlet />
-    </nav>
-  )
-}
-
-
+import Navigation from "./routes/navigation/navigation.component";
+import SignIn from "./routes/sign-in/sign-in.component";
 
 const App = () => {
  return (
     <Routes>
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />}></Route>
+        <Route path='sign-in' element={<SignIn />}></Route>
       </Route>
     </Routes>
   );
---
+++ src/routes/navigation/navigation.component.tsx
@@ -0,0 +1,29 @@
+import {Link, Outlet} from "react-router-dom";
+import "./navigation.styles.scss"
+import {ReactComponent as CrownLogo} from "../../assets/crown_logo.svg";
+
+
+const Navigation = () => {
+  return (
+    <>
+      <nav>
+        <ul className='navigation'>
+          <li>
+            <Link to='/' className='logo-container'>
+              <CrownLogo/>
+            </Link>
+          </li>
+          <div className='nav-links-container'>
+            <li><Link to='/' className='nav-link'>SHOP</Link></li>
+            <li><Link to='/' className='nav-link'>CONTACT</Link></li>
+            <li><Link to='/sign-in' className='nav-link'>SIGN IN</Link></li>
+            <li><Link to='/' className='nav-link'>CART</Link></li>
+          </div>
+        </ul>
+      </nav>
+      <Outlet />
+  </>
+  )
+}
+
+export default Navigation
