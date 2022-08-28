f338672c0908f4144feddd29e839a86f7b436988
geforseN <semen.neb@gmail.com>
2022-08-28 13:27:24 +0700

    refactor checkout to table component and change checkout styles

--- src/routes/checkout/checkout.component.tsx
+++ src/routes/checkout/checkout.component.tsx
@@ -9,31 +9,42 @@ import './checkout.style.scss';
 const Checkout = () => {
   const {cartItems, cartTotal} = useContext(CartContext);
 
-  return (
-    <div className='checkout-container'>
-      <div className='checkout-header'>
-        <div className='header-block'>
-          <span>Product</span>
-        </div>
-        <div className='header-block'>
-          <span>Description</span>
-        </div>
-        <div className='header-block'>
-          <span>Quantity</span>
-        </div>
-        <div className='header-block'>
-          <span>Price</span>
-        </div>
-        <div className='header-block'>
-          <span>Remove</span>
-        </div>
-      </div>
-      {cartItems.map((cartItem) => (
-        // @ts-ignore fix never
-        <CheckoutItem key={cartItem.id} cartItem={cartItem} />
-      ))}
-      <span className='total'>Total: ${cartTotal}</span>
-    </div>
+  return cartItems.length === 0 ? (
+    <div>No items is cart</div>
+    ) : (
+    <table className='checkout-container'>
+      <thead className='checkout-header'>
+        <tr >
+          <th className='header-block'>
+            <span>Product</span>
+          </th>
+          <th className='header-block'>
+            <span>Description</span>
+          </th>
+          <th className='header-block'>
+            <span>Quantity</span>
+          </th>
+          <th className='header-block'>
+            <span>Price</span>
+          </th>
+          <th className='header-block'>
+            <span>Remove</span>
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        {cartItems.map((cartItem) => (
+          // @ts-ignore fix never
+          <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+        ))}
+      </tbody>
+      <tfoot style={{width: '100%'}}>
+        <tr className='total'>
+          <td>Total:</td>
+          <td>${cartTotal}</td>
+        </tr>
+      </tfoot>
+    </table>
   );
 };
 
