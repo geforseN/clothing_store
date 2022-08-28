6dc201afac63e6369f37935c9ab7445dc15f1de8
geforseN <semen.neb@gmail.com>
2022-08-28 13:28:51 +0700

    refactor checkout to table component and change checkout styles

--- src/components/checkout-item/checkout-item.component.tsx
+++ src/components/checkout-item/checkout-item.component.tsx
@@ -3,7 +3,7 @@ import {FC, useContext} from 'react';
 import {CartContext} from '../../contexts/cart.context';
 
 import './checkout-item.style.scss';
-import {cartItem} from "../../contexts/products.context.";
+import {cartItem} from "../../contexts/categories.context";
 
 const CheckoutItem: FC<CheckoutItemProps> = ({ cartItem }) => {
   const {name, imageUrl, price, quantity} = cartItem;
@@ -16,25 +16,28 @@ const CheckoutItem: FC<CheckoutItemProps> = ({ cartItem }) => {
   const clearItemHandler = () => clearItemFromCart(cartItem);
 
   return (
-    <div className='checkout-item-container'>
-      <div className='image-container'>
+    <tr className='checkout-item-container'>
+      <td className='image-container'>
         <img src={imageUrl} alt={`${name}`} />
-      </div>
-      <span className='name'> {name} </span>
-      <span className='quantity'>
-        <div className='arrow' onClick={removeItemHandler}>
-          &#10094;
+      </td>
+      <td className='name'>
+        <span> {name} </span>
+      </td>
+      <td style={{display: 'flex', justifyContent: "center", height: "inherit", alignItems: "center"}}>
+        {}
+        <div className='quantity'>
+          <div className='arrow' onClick={removeItemHandler}>&#10094;</div>
+          <span className='value'>{quantity}</span>
+          <div className='arrow' onClick={addItemHandler}>&#10095;</div>
         </div>
-        <span className='value'>{quantity}</span>
-        <div className='arrow' onClick={addItemHandler}>
-          &#10095;
-        </div>
-      </span>
-      <span className='price'> {price}</span>
-      <div className='remove-button' onClick={clearItemHandler}>
+      </td>
+      <td>
+        <span className='price'> {price}</span>
+      </td>
+      <td className='remove-button' onClick={clearItemHandler}>
         &#10005;
-      </div>
-    </div>
+      </td>
+    </tr>
   );
 };
 
