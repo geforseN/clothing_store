8c76895ef3864df5249f5b20edcc59f103373221
geforseN <semen.neb@gmail.com>
2022-09-03 13:47:24 +0700

    change navigation component state from context to redux

--- src/routes/navigation/navigation.component.tsx
+++ src/routes/navigation/navigation.component.tsx
@@ -1,21 +1,22 @@
 import {Outlet} from "react-router-dom";
-import {useContext} from "react";
-
-import {UserContext} from "../../contexts/user.context";
-import {CartContext} from "../../contexts/cart/cart.context";
 
 import CartIcon from "../../components/cart-icon/cart-icon.component";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 import {ReactComponent as CrownLogo} from "../../assets/crown_logo.svg";
 
-import {signOutUser} from "../../utils/firebase/firebase.utils";
-
 import {NavigationContainer, LogoContainer, NavLinks, NavLink} from "./navigation.styles"
+import {useDispatch, useSelector} from "react-redux";
+import {selectCurrentUser} from "../../store/user/user.selector";
+import {selectIsCartOpen} from "../../store/cart/cart.selector";
+import {signOutStart} from "../../store/user/user.action";
 
 
 const Navigation = () => {
-  const {currentUser} = useContext(UserContext);
-  const {isCartOpen} = useContext(CartContext);
+  const dispatch = useDispatch();
+  const currentUser = useSelector(selectCurrentUser);
+  const isCartOpen = useSelector(selectIsCartOpen);
+
+  const signOutUser = () => dispatch(signOutStart());
 
   return (
     <>
