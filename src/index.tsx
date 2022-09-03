5fcb71b7717d8970e408a39623a4aa791daada4e
geforseN <semen.neb@gmail.com>
2022-09-03 13:46:04 +0700

    add redux, redux persist, redux logger, redux saga, reselect

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
 import {createRoot} from 'react-dom/client';
 import {BrowserRouter} from "react-router-dom";
+import {Provider} from "react-redux"
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {UserProvider} from "./contexts/user.context";
-import {CategoriesProvider} from "./contexts/categories.context";
-import {CartProvider} from "./contexts/cart/cart.context";
+import {store, persistor} from "./store/store"
+import {PersistGate} from "redux-persist/integration/react";
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <UserProvider>
-          <CategoriesProvider>
-            <CartProvider>
-              <App />
-            </CartProvider>
-          </CategoriesProvider>
-      </UserProvider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <PersistGate loading={'loading'} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
 
